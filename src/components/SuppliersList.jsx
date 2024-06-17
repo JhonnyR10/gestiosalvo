347be629db0e040adcf4258b269d5ba0eee9f32f
Giovanni Longo <giovanni.longo10@icloud.com>
2024-06-17 22:54:58 +0200

    Cambio colore tasti e/d

--- src/components/SuppliersList.jsx
+++ src/components/SuppliersList.jsx
@@ -89,22 +89,22 @@ const SupplierList = () => {
                       </p>
                     </div>
                     <div className="d-flex justify-content-center align-items-center">
-                      <div className="col-6 border-end bg-danger py-2">
+                      <div className="col-6 border-end py-2">
                         <div
                           className="d-flex justify-content-center align-items-center "
                           onClick={() => handleShowDelete(supplier)}
                         >
-                          <Trash className="col-4 text-white"></Trash>
-                          <p className="m-0 col-8 text-white">Elimina</p>
+                          <Trash className="col-4 text-danger"></Trash>
+                          <p className="m-0 col-8 text-danger">Elimina</p>
                         </div>
                       </div>
-                      <div className="col-6 bg-warning py-2">
+                      <div className="col-6 py-2">
                         <div
                           className="d-flex justify-content-center align-items-center "
                           onClick={() => handleShowEdit(supplier)}
                         >
-                          <Pencil className="col-4"></Pencil>
-                          <p className="m-0 col-8">Modifica</p>
+                          <Pencil className="col-4 text-warning"></Pencil>
+                          <p className="m-0 col-8 text-warning">Modifica</p>
                         </div>
                       </div>
                     </div>
